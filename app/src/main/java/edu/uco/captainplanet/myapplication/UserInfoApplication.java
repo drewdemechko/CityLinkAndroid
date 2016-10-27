29ab159c525e07763e338865d618364746c4af70
Derek Renfro <drenfro87@gmail.com>
2016-10-27 13:40:59 -0500

    removd uneeded toast message

--- app/src/main/java/edu/uco/captainplanet/myapplication/UserInfoApplication.java
+++ app/src/main/java/edu/uco/captainplanet/myapplication/UserInfoApplication.java
@@ -1,5 +1,18 @@
 package edu.uco.captainplanet.myapplication;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
+import edu.uco.captainplanet.myapplication.Dtos.Favorite;
+import edu.uco.captainplanet.myapplication.Dtos.Notification;
+
 public class UserInfoApplication {
     private static UserInfoApplication instance;
 
@@ -7,6 +20,8 @@ public class UserInfoApplication {
     private String password;
     private boolean loggedIn;
     private int id;
+    private List<Favorite> myFavorites;
+    List<Notification> userSpecificNotifications;
 
     public static synchronized UserInfoApplication getInstance() {
         if (instance == null) {
@@ -19,6 +34,8 @@ public class UserInfoApplication {
         setUsername("");
         setPassword("");
         setLoggedIn(false);
+        myFavorites = new ArrayList<>();
+        userSpecificNotifications = new ArrayList<>();
     }
 
     public int getId(){ return id; }
@@ -44,4 +61,47 @@ public class UserInfoApplication {
     public boolean isLoggedIn() { return loggedIn; }
 
     public void setLoggedIn(boolean loggedIn) { this.loggedIn = loggedIn; }
+
+    public List<Favorite> getFavorites(){ return myFavorites; }
+
+    public void getUserFavorites()
+    {
+         /*
+         * Attempt to get JSON info
+         * Reference: http://loopj.com/android-async-http/
+        */
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.setTimeout(5000); // give enough time for client to get the JSON data
+        client.get("https://uco-edmond-bus.herokuapp.com/api/favoriteservice/favorites/"
+                        + username
+                , new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                        // Called when response HTTP status is "200 OK"
+                        try {
+                            setUserFavorites(response);
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                });
+    }
+
+    private void setUserFavorites(JSONArray response)
+    {
+        try {
+            for (int i = 0; i < response.length(); i++) {
+                JSONObject item = new JSONObject(response.getString(i));
+                Favorite newFavorite = new Favorite(item.getInt("id"), item.getInt("userId"), item.getInt("favoriteId"), item.getString("type"), item.getString("name"));
+                getFavorites().add(newFavorite);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public List<Notification> getUserNotifications()
+    {
+        return userSpecificNotifications;
+    }
 }
