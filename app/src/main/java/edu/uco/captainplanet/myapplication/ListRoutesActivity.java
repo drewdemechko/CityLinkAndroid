e4836a7997454ac76118da895d99369c23f3e10b
SimpleSandman <simple.sandman@gmail.com>
2016-11-02 17:58:49 -0500

    Modified List Routes Activity

--- app/src/main/java/edu/uco/captainplanet/myapplication/ListRoutesActivity.java
+++ app/src/main/java/edu/uco/captainplanet/myapplication/ListRoutesActivity.java
@@ -1,12 +1,105 @@
 package edu.uco.captainplanet.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
 
-public class ListRoutesActivity extends AppCompatActivity {
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class ListRoutesActivity extends Activity {
+    private Routes routes;
+    private ArrayList<BusStop> stops;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_routes);
+
+        routes = new Routes();
+        stops = new ArrayList<>();
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.setTimeout(5000);
+        client.get("https://uco-edmond-bus.herokuapp.com/api/busstopservice/stops"
+                , new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONArray theStops) {
+                        setStops(theStops);
+                    }
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                        // do nothing
+                    }
+                });
+    }
+
+    public void setRoutes(JSONArray theRoutes)
+    {
+        try {
+            for (int x = 0; x < theRoutes.length(); x++)
+            {
+                Log.d("theApp","forLoop");
+                Route currentRoute = new Route();
+                JSONObject routeJSON = theRoutes.getJSONObject(x);
+                if (routeJSON.has("name")) {
+                    currentRoute.setName(routeJSON.getString("name"));
+                }
+                if (routeJSON.has("id")) {
+                    currentRoute.setId(routeJSON.getInt("id"));
+                }
+                if (routeJSON.has("busStops")) {
+                    currentRoute.setOrderedStops(routeJSON.getJSONArray("busStops"), stops);
+                }
+                routes.addRoute(currentRoute);
+            }
+        } catch (JSONException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void setStops(JSONArray theStops)
+    {
+        try {
+            for (int x = 0; x < theStops.length(); x++)
+            {
+                Log.d("theApp","forLoop");
+                BusStop currentStop = new BusStop();
+
+                if (theStops.getJSONObject(x).has("name")) {
+                    currentStop.setName(theStops.getJSONObject(x).getString("name"));
+                }
+                stops.add(currentStop);
+            }
+        } catch (JSONException ex) {
+            ex.printStackTrace();
+        }
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.setTimeout(5000);
+        client.get("https://uco-edmond-bus.herokuapp.com/api/routeservice/routes"
+                , new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONArray theRoutes) {
+                        setRoutes(theRoutes);
+
+                        RouteArrayAdapter adapter = new RouteArrayAdapter(getApplicationContext(), routes.getRoutes());
+                        ListView listView = (ListView) findViewById(R.id.lvRoutes);
+                        listView.setAdapter(adapter);
+                    }
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                        // do nothing
+                    }
+                });
     }
 }
--- app/src/main/java/edu/uco/captainplanet/myapplication/RouteArrayAdapter.java
+++ app/src/main/java/edu/uco/captainplanet/myapplication/RouteArrayAdapter.java
@@ -1,8 +1,72 @@
 package edu.uco.captainplanet.myapplication;
 
-/**
- * Created by kento on 11/1/2016.
- */
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-public class RouteArrayAdapter {
+import java.util.ArrayList;
+
+public class RouteArrayAdapter extends ArrayAdapter<Route> {
+    /*
+    private final Context context;
+    private final String[] values;
+
+    public RouteArrayAdapter(Context context, String[] values) {
+        super(context, R.layout.activity_routes, values);
+        this.context = context;
+        this.values = values;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(R.layout.activity_routes, parent, false);
+        TextView textView = (TextView) rowView.findViewById(R.id.label);
+        textView.setText(values[position]);
+
+        return rowView;
+    }
+    */
+
+    // View lookup cache
+    private static class ViewHolder {
+        TextView name;
+        TextView home;
+    }
+
+    public RouteArrayAdapter(Context context, ArrayList<Route> routes) {
+        super(context, R.layout.item_route, routes);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        Route route = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        ViewHolder viewHolder; // view lookup cache stored in tag
+        if (convertView == null) {
+            // If there's no view to re-use, inflate a brand new view for row
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_route, parent, false);
+            viewHolder.name = (TextView) convertView.findViewById(R.id.tvName);
+            //viewHolder.home = (TextView) convertView.findViewById(R.id.tvHometown);
+            // Cache the viewHolder object inside the fresh view
+            convertView.setTag(viewHolder);
+        } else {
+            // View is being recycled, retrieve the viewHolder object from tag
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        // Populate the data from the data object via the viewHolder object
+        // into the template view.
+        viewHolder.name.setText(route.getName());
+        //viewHolder.home.setText(route.getId());
+        // Return the completed view to render on screen
+        return convertView;
+    }
 }
