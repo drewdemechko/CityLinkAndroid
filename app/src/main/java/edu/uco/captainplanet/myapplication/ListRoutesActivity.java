232b196a87ced88e583be16a74b13082ba0ce643
SimpleSandman <simple.sandman@gmail.com>
2016-11-01 22:37:02 -0500

    Added List Routes Activity

---
+++ app/src/main/java/edu/uco/captainplanet/myapplication/ListRoutesActivity.java
@@ -0,0 +1,12 @@
+package edu.uco.captainplanet.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListRoutesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/edu/uco/captainplanet/myapplication/RouteArrayAdapter.java
@@ -0,0 +1,8 @@
+package edu.uco.captainplanet.myapplication;
+
+/**
+ * Created by kento on 11/1/2016.
+ */
+
+public class RouteArrayAdapter {
+}
