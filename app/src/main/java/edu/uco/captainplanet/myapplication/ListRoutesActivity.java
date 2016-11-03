c9f3264bc50a292470b2f220021ae9a052859ce3
SimpleSandman <simple.sandman@gmail.com>
2016-11-02 21:00:26 -0500

    Finished loading routes by buses

--- app/src/main/java/edu/uco/captainplanet/myapplication/ListRoutesActivity.java
+++ app/src/main/java/edu/uco/captainplanet/myapplication/ListRoutesActivity.java
@@ -15,14 +15,19 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import cz.msebera.android.httpclient.Header;
 
 public class ListRoutesActivity extends ListActivity {
     private Routes routes;
     private ArrayList<BusStop> stops;
+    private ArrayList<Bus> buses;
 
     private ListView listView;
     private List<ListRowItem> rowItems;
@@ -34,6 +39,7 @@ public class ListRoutesActivity extends ListActivity {
 
         routes = new Routes();
         stops = new ArrayList<>();
+        buses = new ArrayList<>();
 
         AsyncHttpClient client = new AsyncHttpClient();
         client.setTimeout(5000);
@@ -62,33 +68,15 @@ public class ListRoutesActivity extends ListActivity {
 
     public void setListRowItems() {
         rowItems = new ArrayList<>();
-        for (int i = 0; i < routes.getRoutes().size(); i++) {
-            ListRowItem item = new ListRowItem("Bus " + i, routes.getRoutes().get(i).getName());
-            rowItems.add(item);
-        }
-    }
-
-    public void setRoutes(JSONArray theRoutes)
-    {
-        try {
-            for (int x = 0; x < theRoutes.length(); x++)
-            {
-                Log.d("setRoutes","forLoop");
-                Route currentRoute = new Route();
-                JSONObject routeJSON = theRoutes.getJSONObject(x);
-                if (routeJSON.has("name")) {
-                    currentRoute.setName(routeJSON.getString("name"));
+        for (int i = 0; i < buses.size(); i++) {
+            // match buses to routes
+            for (int j = 0; j < routes.getRoutes().size(); j++) {
+                if (buses.get(i).getRoute().equals(routes.getRoutes().get(j).getName())) {
+                    ListRowItem item = new ListRowItem(buses.get(i).getName(), routes.getRoutes().get(j).getName());
+                    rowItems.add(item);
+                    break;
                 }
-                if (routeJSON.has("id")) {
-                    currentRoute.setId(routeJSON.getInt("id"));
-                }
-                if (routeJSON.has("busStops")) {
-                    currentRoute.setOrderedStops(routeJSON.getJSONArray("busStops"), stops);
-                }
-                routes.addRoute(currentRoute);
             }
-        } catch (JSONException ex) {
-            ex.printStackTrace();
         }
     }
 
@@ -116,7 +104,96 @@ public class ListRoutesActivity extends ListActivity {
                     @Override
                     public void onSuccess(int statusCode, Header[] headers, JSONArray theRoutes) {
                         setRoutes(theRoutes);
+                    }
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                        // do nothing
+                    }
+                });
+    }
+
+    public void setRoutes(JSONArray theRoutes)
+    {
+        try {
+            for (int x = 0; x < theRoutes.length(); x++)
+            {
+                Log.d("setRoutes","forLoop");
+                Route currentRoute = new Route();
+                JSONObject routeJSON = theRoutes.getJSONObject(x);
+                if (routeJSON.has("name")) {
+                    currentRoute.setName(routeJSON.getString("name"));
+                }
+                if (routeJSON.has("id")) {
+                    currentRoute.setId(routeJSON.getInt("id"));
+                }
+                if (routeJSON.has("busStops")) {
+                    currentRoute.setOrderedStops(routeJSON.getJSONArray("busStops"), stops);
+                }
+                routes.addRoute(currentRoute);
+            }
+        } catch (JSONException ex) {
+            ex.printStackTrace();
+        }
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.setTimeout(5000);
+        client.get("https://uco-edmond-bus.herokuapp.com/api/busservice/buses"
+                , new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONArray theStops) {
+                        setBuses(theStops);
+                    }
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                        // do nothing
+                    }
+                });
+    }
+
+    public void setBuses(JSONArray theBuses)
+    {
+        try {
+            for (int x = 0; x < theBuses.length(); x++)
+            {
+                Log.d("setBuses","forLoop");
+                Bus currentBus = new Bus();
+                JSONObject busJSON = theBuses.getJSONObject(x);
+
+                if (busJSON.has("name")) {
+                    currentBus.setName(busJSON.getString("name"));
+                }
+                if (busJSON.has("driver")) {
+                    currentBus.setDriver(busJSON.getString("driver"));
+                }
+                if (busJSON.has("route")) {
+                    currentBus.setRoute(busJSON.getString("route"));
+                }
+                if (busJSON.has("lastStop")) {
+                    currentBus.setLastStop(busJSON.getString("lastStop"));
+                }
+                if (busJSON.has("active")) {
+                    currentBus.setActive(busJSON.getBoolean("active"));
+                }
+                if (busJSON.has("lastActive")) {
+                    String dateStr = busJSON.getString("lastActive");
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+                    Date lastActiveDate = sdf.parse(dateStr);
+                    currentBus.setLastActive(lastActiveDate);
+                }
+                buses.add(currentBus);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.setTimeout(5000);
+        client.get("https://uco-edmond-bus.herokuapp.com/api/busservice/buses"
+                , new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONArray theStops) {
                         setListRowItems();
+
                         RouteArrayAdapter adapter = new RouteArrayAdapter(getApplicationContext(), rowItems);
                         listView = (ListView) findViewById(android.R.id.list);
                         listView.setAdapter(adapter);
